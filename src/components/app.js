98cc1e1075e704ca8563ab270e0538b47ef083f3
Meg Galvez <meg@oakmaine.com>
2019-12-11 13:48:19 -0700

    initial commit

---
+++ src/components/app.js
@@ -0,0 +1,18 @@
+// class App {
+
+//     constructor(){
+//         this.initBindingsandEventListeners()
+//         this.renderPage(new Signup(this.page))
+
+//     }
+
+//     initBindingsandEventListeners(){
+//         this.container = document.querySelector('#app-container')
+//         this.navbar = document.querySelector('.nav-container')
+//         this.page = document.querySelector('#page-container')
+//     }
+
+//     renderPage(page){
+//         page.render() 
+//     }
+// }
---
+++ src/components/pageManagers/signup.js
@@ -0,0 +1,56 @@
+// class Signup {
+
+//     constructor(container) {
+//         this.container = container
+//     }
+
+
+//     get staticHTML(){
+//         return(`<div class="bg-contact3">
+       
+//         <div class="container-contact3">
+//           <div class="wrap-contact3">
+//             <form class="contact3-form validate-form">
+//               <span class="contact3-form-title">
+//                 Sign Up
+//               </span>
+      
+//               <div class="wrap-input3 validate-input" data-validate="First name is required">
+//                 <input class="input3" type="text" name="first_name" id="first_name" placeholder="First Name">
+//                 <span class="focus-input3"></span>
+//               </div>
+      
+//               <div class="wrap-input3 validate-input" data-validate="Last name is required">
+//                   <input class="input3" type="text" name="last_name" id="last_name" placeholder="Last Name">
+//                   <span class="focus-input3"></span>
+//                 </div>
+      
+//               <div class="wrap-input3 validate-input" data-validate = "Valid email is required: ex@abc.xyz">
+//                 <input class="input3" type="text" name="email" placeholder="Your Email">
+//                 <span class="focus-input3"></span>
+//               </div>
+      
+//               <div class="wrap-input3 validate-input" data-validate = "Password is required">
+//                   <input class="input3" type="password" name="password" id="password" placeholder="Password ">
+//                   <span class="focus-input3"></span>
+//                 </div>
+      
+//               <div class="container-contact3-form-btn">
+//                 <button class="contact3-form-btn">
+//                   Submit
+//                 </button>
+//               </div>
+//             </form>
+//           </div>
+//         </div>
+//       </div>
+//         `)
+//     }
+//     render(){
+//         this.container.innerHTML = this.staticHTML
+//     }
+// }
+
+
+
+
